a4f3a0caafad9bf9e6d878bf0b9861131b85e047
Rahmat Ullah (Anik) <49795957+RahmatUllahAnik@users.noreply.github.com>
2019-07-11 03:41:25 +0600

    Create NotificationView.java

---
+++ Notification/NotificationView.java
@@ -0,0 +1,11 @@
+package com.example.notificationdemo;
+import android.os.Bundle;
+import android.app.Activity;
+public class NotificationView extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
